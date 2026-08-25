315a555388b56e51c8f4fc64ab37038ff6d4fafa
frankmambimbo-beep <frankmambimbo@gmail.com>
2026-08-25 22:35:46 +0200

    Introduce BodyGoalSelection class
    Added BodyGoalSelection class to manage body type goals.

--- lib/features/bodygoal/domain/body_type_goal.dart
+++ lib/features/bodygoal/domain/body_type_goal.dart
@@ -5,23 +5,34 @@ part 'body_type_goal.g.dart';
 @HiveType(typeId: 60)
 enum BodyTypeGoal {
   @HiveField(0)
-  vTaper, // broad shoulders, narrow waist
+  vTaper,
   @HiveField(1)
-  bulk, // overall size and mass
+  bulk,
   @HiveField(2)
-  leanToned, // low body fat, defined muscle
+  leanToned,
   @HiveField(3)
-  cylinder, // balanced, straight-up-and-down frame
+  cylinder,
   @HiveField(4)
-  hourglass, // curves, balanced upper/lower with a defined waist
+  hourglass,
   @HiveField(5)
-  athletic, // general balanced athletic build
+  athletic,
   @HiveField(6)
-  pear, // fuller lower body, want to build/balance upper body
+  pear,
   @HiveField(7)
-  apple, // weight carried in the midsection, want a trimmer waist
+  apple,
   @HiveField(8)
-  diamond, // weight carried centrally, narrower shoulders/hips
+  diamond,
   @HiveField(9)
-  spoon, // fuller hips/thighs with a defined waist
+  spoon,
+}
+
+@HiveType(typeId: 61)
+class BodyGoalSelection extends HiveObject {
+  @HiveField(0)
+  BodyTypeGoal goal;
+
+  @HiveField(1)
+  DateTime selectedAt;
+
+  BodyGoalSelection({required this.goal, required this.selectedAt});
 }
